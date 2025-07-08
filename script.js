5434f48b7d3f966ca680a753fabd8fb7d84b3411
nsmuttoo <nsmuttoo@gmail.com>
2025-07-08 17:13:57 -0400

    Weather app is completed

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+//console.log(fetchData("toronto"))
+
 updateDisplay()
 
 async function fetchData(location) {
@@ -28,16 +30,93 @@ async function processData(location) {
 }
 
 async function updateDisplay() {
+    const content = document.getElementById("content")
     const inputBox = document.createElement("input")
     const submitButton = document.createElement("button")
+    const dataBox = document.createElement("div")
+    submitButton.innerHTML = "Submit"
+    const toggleFlag = document.createElement("button")
+    toggleFlag.innerText = "F/C"
+
+    toggleFlag.addEventListener("click",toggleFlagOnClick)
+
+    function toggleFlagOnClick(){
+        if (flag ==0){
+            flag = 1
+        }else {
+            flag = 0
+        }
+        displayData()
+    }
+
+    let flag = 0
+    
+    let data = 0
 
     submitButton.addEventListener("click",submitButtonOnClick)
 
-    document.body.appendChild(inputBox)
-    document.body.appendChild(submitButton)
+    content.appendChild(inputBox)
+    content.appendChild(submitButton)
+    
+    content.appendChild(dataBox)
+    content.appendChild(toggleFlag)
 
     async function submitButtonOnClick() {
-        console.log(processData(inputBox.value))
+        try{
+        data =  await processData(inputBox.value)
+        
+        console.log(data)
+        
+        updateBackground(data.temp)
+        displayData()
+    }catch(error){
+        alert("Sorry we couldn't find this location")
+        inputBox.value = ""
+    }
+    }
+
+    function displayData(){
+        dataBox.innerHTML = ""
+
+        const location = document.createElement("div")
+        const temp = document.createElement("div")
+        const conditions = document.createElement("div")
+
+        location.innerText = data.location
+        conditions.innerText = data.conditions
+
+        if(flag ==0){
+            store = (data.temp-32)*5/9
+            store = Math.trunc(store)
+            temp.innerText =  store + " C"
+
+        }else{
+            temp.innerText=data.temp + " F"
+        }
+        dataBox.appendChild(location)
+        dataBox.appendChild(conditions)
+        dataBox.appendChild(temp)
+
+        
+
+
+    }
+
+    function updateBackground(temp){
+        content.classList = ""
+        
+        if(temp > 90){
+            content.classList.add("hot")
+        }else if(temp > 70){
+            
+            content.classList.add("warm")
+        }else if (temp > 50){
+            content.classList.add("cool")
+        }else if (temp <= 50){
+            content.classList.add("cold")
+        }
+
+
     }
 
 
